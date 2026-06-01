e748d84c2955e42055ec317e7fc4327c134add56
Honghao Zhang <m@honghao.dev>
2026-05-31 18:02:55 -0700

    clean up

--- Scripts/chouti-sync-upstream.sh
+++ Scripts/chouti-sync-upstream.sh
@@ -81,8 +81,8 @@ verify_upstream() {
 
 configure_git() {
     if [[ -z "$(git config user.email || true)" ]]; then
-        git config user.email "github-actions[bot]@users.noreply.github.com"
-        git config user.name "github-actions[bot]"
+        git config user.email "m@honghao.dev"
+        git config user.name "Honghao Zhang"
     fi
 }
 
